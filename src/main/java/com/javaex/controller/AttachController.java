db4d09a159bc6ddc1da7fe782fbb5f7ef7155e40
황덕룡 <hwangsee12@naver.com>
2024-09-27 11:19:28 +0900

    난 커밋이 참 힘들어...ㅠ

--- src/main/java/com/javaex/controller/AttachController.java
+++ src/main/java/com/javaex/controller/AttachController.java
@@ -1,7 +1,9 @@
 package com.javaex.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.javaex.util.JsonResult;
 
@@ -9,9 +11,9 @@ import com.javaex.util.JsonResult;
 public class AttachController {
 	
 	@PostMapping("api/attachs")
-	public JsonResult form() {
+	public JsonResult form(@RequestParam("profileImg")MultipartFile profileImg) {
 		System.out.println("AttachController.form()");
-		
+		System.out.println(profileImg.getOriginalFilename());
 		return JsonResult.success("");
 	}
 }
